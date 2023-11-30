ced0ba5f9191a0d909e31551d32f403ca45a91b8
bdbrink <brendan.brink@outlook.com>
2023-11-30 17:00:12 -0600

    add cross validation of iris data set

---
+++ cross_validation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from sklearn import datasets
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.svm import SVC
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data  # Features
+y = iris.target  # Target variable (labels)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Support Vector Machine (SVM) classifier
+svm_classifier = SVC(kernel='linear', C=1)
+
+# Perform k-fold cross-validation (k=5)
+cv_scores = cross_val_score(svm_classifier, X_train, y_train, cv=5)
+
+# Print the cross-validation scores
+print("Cross-Validation Scores:", cv_scores)
+
+# Print the average cross-validation score
+average_cv_score = np.mean(cv_scores)
+print("Average Cross-Validation Score:", average_cv_score)
+
+# Train the classifier on the full training set
+svm_classifier.fit(X_train, y_train)
+
+# Evaluate the accuracy on the test set
+test_accuracy = svm_classifier.score(X_test, y_test)
+print(f"Accuracy on Test Set: {test_accuracy * 100:.2f}%")
